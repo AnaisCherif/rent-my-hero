96c434db819efa7b61ae911371dc81648b3182c1
Ali Ripault <ripault.ali@gmail.com>
2024-05-27 14:28:41 +0200

    app hero

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,7 @@
 class Character < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+
+  validates :name, :location, :price, :photo_url, presence: true
+  validates :name, uniqueness: true
 end
