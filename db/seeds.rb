57f859fd9387fd6c49c6d1fb0a7768afa40cfba2
Anais Cherif <a.cherif@orange.fr>
2024-05-27 15:55:01 +0200

    show character

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,26 +12,29 @@ require 'faker'
 puts "Start"
 
 Character.destroy_all
+User.destroy_all
+
+user = User.create!(email: 'test@test.com', password: 'azerty')
 
 3.times do
-  Character.create(
+  Character.create!(
     name: Faker::TvShows::Simpsons.character,
     from: "The Simpsons",
     location: "Springfield",
     price: rand(50..1000),
     skills: ["#{Faker::Job.key_skill}, #{Faker::Hobby.activity}"],
-    user_id: 1
-  )
-end
+    user_id: user.id
+    )
+  end
 
-3.times do
-  Character.create(
+  3.times do
+    Character.create!(
     name: [Faker::DcComics.hero, Faker::DcComics.heroine, Faker::DcComics.villain].sample,
     from: "DC Comics",
     location: Faker::Nation.capital_city,
     price: rand(50..1000),
     skills: ["#{Faker::Job.key_skill}, #{Faker::Hobby.activity}"],
-    user_id: 1
+    user: user
   )
 end
 
