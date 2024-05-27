e9a58a2f96c222bc3278c761ad8e57f16c29e0f0
Ali Ripault <ripault.ali@gmail.com>
2024-05-27 14:15:39 +0200

    app hero

---
+++ db/migrate/20240527120950_create_characters.rb
@@ -0,0 +1,15 @@
+class CreateCharacters < ActiveRecord::Migration[7.1]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.string :location
+      t.string :from
+      t.integer :price
+      t.text :skills
+      t.string :photo_url
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
