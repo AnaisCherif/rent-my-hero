4b168991e6b6c5fa19fb15d6b75ccff752705245
Ali Ripault <ripault.ali@gmail.com>
2024-05-28 14:43:35 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,27 +15,42 @@ puts "Start"
 Character.destroy_all
 User.destroy_all
 
-user = User.create!(email: "test@test.com", password: "azerty")
+characters = [
+  { name: "Harry Potter", location: "Hogwarts", from: "Harry Potter Series" },
+  { name: "Frodo Baggins", location: "Shire", from: "The Lord of the Rings" },
+  { name: "Jon Snow", location: "Winterfell", from: "Game of Thrones" },
+  { name: "Luke Skywalker", location: "Tatooine", from: "Star Wars" },
+  { name: "Hermione Granger", location: "Hogwarts", from: "Harry Potter Series" },
+  { name: "Aragorn", location: "Gondor", from: "The Lord of the Rings" },
+  { name: "Daenerys Targaryen", location: "Dragonstone", from: "Game of Thrones" },
+  { name: "Arya Stark", location: "Winterfell", from: "Game of Thrones" },
+  { name: "Leia Organa", location: "Alderaan", from: "Star Wars" },
+  { name: "Darth Vader", location: "Death Star", from: "Star Wars" },
+  { name: "Yoda", location: "Dagobah", from: "Star Wars" },
+  { name: "Indiana Jones", location: "Various Locations", from: "Indiana Jones" },
+  { name: "Sherlock Holmes", location: "221B Baker Street", from: "Sherlock Holmes" },
+  { name: "Han Solo", location: "Millennium Falcon", from: "Star Wars" },
+  { name: "Gandalf", location: "Middle-earth", from: "The Lord of the Rings" },
+  { name: "Cersei Lannister", location: "King's Landing", from: "Game of Thrones" },
+  { name: "Neo", location: "Zion", from: "The Matrix" },
+  { name: "Trinity", location: "Zion", from: "The Matrix" },
+  { name: "Morpheus", location: "Zion", from: "The Matrix" },
+  { name: "Tony Stark", location: "Stark Tower", from: "Iron Man" },
+  { name: "Thor", location: "Asgard", from: "Thor" },
+  { name: "Peter Parker", location: "New York", from: "Spider-Man" },
+  { name: "Clark Kent", location: "Metropolis", from: "Superman" },
+  { name: "Bruce Wayne", location: "Gotham City", from: "Batman" },
+]
 
-3.times do
+user = User.create!(email: "test@test.com", password: "azerty", first_name: "Test", last_name: "Test")
+characters.each do |character|
   Character.create!(
-    name: Faker::TvShows::Simpsons.character,
-    from: "The Simpsons",
-    location: "Springfield",
+    name: character[:name],
+    location: character[:location],
+    from: character[:from],
     price: rand(50..1000),
     skills: ["#{Faker::Job.key_skill}, #{Faker::Hobby.activity}"],
     user_id: user.id
-  )
-end
-
-3.times do
-  Character.create!(
-    name: [Faker::DcComics.hero, Faker::DcComics.heroine, Faker::DcComics.villain].sample,
-    from: "DC Comics",
-    location: Faker::Nation.capital_city,
-    price: rand(50..1000),
-    skills: ["#{Faker::Job.key_skill}, #{Faker::Hobby.activity}"],
-    user: user
   )
 end
 
