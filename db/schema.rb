30371a1e38555d22663a18218dc5ed384863b063
Anais Cherif <a.cherif@orange.fr>
2024-05-30 12:00:39 +0200

    search ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_29_145356) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_30_085858) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,7 +57,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_29_145356) do
   create_table "characters", force: :cascade do |t|
     t.string "name"
     t.string "location"
-    t.string "from"
+    t.string "univers"
     t.integer "price"
     t.text "skills"
     t.string "photo_url"
